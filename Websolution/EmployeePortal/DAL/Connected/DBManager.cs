bfd0ade9f1b6de21c346c4c966d7f7607240d412
Ameya Kulkarni <ameyak2001@gmail.com>
2024-01-04 23:36:12 +0530

    final changes

--- Websolution/EmployeePortal/DAL/Connected/DBManager.cs
+++ Websolution/EmployeePortal/DAL/Connected/DBManager.cs
@@ -136,5 +136,27 @@ public static class DBManager
         }
         return false;
     }
+    public static bool DeleteEmployee(int id)
+    {
+        MySqlConnection con = new MySqlConnection();
+        con.ConnectionString = constring;
+        string query = "delete from employee where id = @id";
+        MySqlCommand cmd = new MySqlCommand(query,con);
+        try{
+            con.Open();
+            cmd.Parameters.AddWithValue("@id",id);
+            int n = cmd.ExecuteNonQuery();
+            if(n>0){
+                return true;
+            }
+
+        }catch(Exception e){
+            Console.WriteLine(e.Message);
+        }finally{
+            con.Close();
+        }
+        return false;
+
+    }
 
 }
